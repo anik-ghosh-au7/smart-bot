12f184a2a525ac1b6680e871cbb1849f312eafef
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-05-02 20:37:54 +0530

    fix: dance model texture

--- src/models/DanceModel.js
+++ src/models/DanceModel.js
@@ -21,43 +21,43 @@ export default function DanceModel({ ...props }) {
 					<skinnedMesh
 						name="Wolf3D_Body"
 						geometry={nodes.Wolf3D_Body.geometry}
-						material={materials['Wolf3D_Body.001']}
+						material={materials['Wolf3D_Body']}
 						skeleton={nodes.Wolf3D_Body.skeleton}
 					/>
 					<skinnedMesh
 						name="Wolf3D_Glasses"
 						geometry={nodes.Wolf3D_Glasses.geometry}
-						material={materials['Wolf3D_Glasses.001']}
+						material={materials['Wolf3D_Glasses']}
 						skeleton={nodes.Wolf3D_Glasses.skeleton}
 					/>
 					<skinnedMesh
 						name="Wolf3D_Hair"
 						geometry={nodes.Wolf3D_Hair.geometry}
-						material={materials['Wolf3D_Hair.001']}
+						material={materials['Wolf3D_Hair']}
 						skeleton={nodes.Wolf3D_Hair.skeleton}
 					/>
 					<skinnedMesh
 						name="Wolf3D_Outfit_Bottom"
 						geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
-						material={materials['Wolf3D_Outfit_Bottom.001']}
+						material={materials['Wolf3D_Outfit_Bottom']}
 						skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
 					/>
 					<skinnedMesh
 						name="Wolf3D_Outfit_Footwear"
 						geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
-						material={materials['Wolf3D_Outfit_Footwear.001']}
+						material={materials['Wolf3D_Outfit_Footwear']}
 						skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
 					/>
 					<skinnedMesh
 						name="Wolf3D_Outfit_Top"
 						geometry={nodes.Wolf3D_Outfit_Top.geometry}
-						material={materials['Wolf3D_Outfit_Top.001']}
+						material={materials['Wolf3D_Outfit_Top']}
 						skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
 					/>
 					<skinnedMesh
 						name="EyeLeft"
 						geometry={nodes.EyeLeft.geometry}
-						material={materials['Wolf3D_Eye.001']}
+						material={materials['Wolf3D_Eye']}
 						skeleton={nodes.EyeLeft.skeleton}
 						morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
 						morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
@@ -65,7 +65,7 @@ export default function DanceModel({ ...props }) {
 					<skinnedMesh
 						name="EyeRight"
 						geometry={nodes.EyeRight.geometry}
-						material={materials['Wolf3D_Eye.001']}
+						material={materials['Wolf3D_Eye']}
 						skeleton={nodes.EyeRight.skeleton}
 						morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
 						morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
@@ -73,7 +73,7 @@ export default function DanceModel({ ...props }) {
 					<skinnedMesh
 						name="Wolf3D_Head"
 						geometry={nodes.Wolf3D_Head.geometry}
-						material={materials['Wolf3D_Skin.001']}
+						material={materials['Wolf3D_Skin']}
 						skeleton={nodes.Wolf3D_Head.skeleton}
 						morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
 						morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
@@ -81,7 +81,7 @@ export default function DanceModel({ ...props }) {
 					<skinnedMesh
 						name="Wolf3D_Teeth"
 						geometry={nodes.Wolf3D_Teeth.geometry}
-						material={materials['Wolf3D_Teeth.001']}
+						material={materials['Wolf3D_Teeth']}
 						skeleton={nodes.Wolf3D_Teeth.skeleton}
 						morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
 						morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
